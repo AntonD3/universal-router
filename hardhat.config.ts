99b37b8d47bf5adbfd4958b37efa9e83abfededd
Vakhtanh Chikhladze <40902039+Pozzitron1337@users.noreply.github.com>
2023-08-17 19:26:30 +0300

    Tests adaptation (#3)
    Co-authored-by: dmitriyischenko <124681031+DmitriyIschenko@users.noreply.github.com>
    Co-authored-by: AntonD3 <antonv.dyadyuk@gmail.com>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,21 +1,25 @@
 import 'hardhat-typechain'
 import '@nomiclabs/hardhat-ethers'
-import '@nomiclabs/hardhat-waffle'
 import '@matterlabs/hardhat-zksync-deploy'
 import '@matterlabs/hardhat-zksync-solc'
 import '@matterlabs/hardhat-zksync-verify'
+import '@matterlabs/hardhat-zksync-chai-matchers'
 import { task } from 'hardhat/config'
 import deployZkSyncEra from './script/deploy_zksync_era'
 import dotenv from 'dotenv'
 dotenv.config()
 
+const ZK_SYNC_LOCALHOST = "http://localhost:8011"
+// This variable is used by the hardhat-zksync-chai-matchers
+process.env.ZKSYNC_WEB3_API_URL = ZK_SYNC_LOCALHOST
+
 task('deployZkSyncEra')
-    .addParam('privateKey', 'Private key used to deploy')
-    .addParam('params', "Path to params json")
-    .addFlag('verify', 'Whether verify the contracts')
-    .setAction(async (taskArgs) => {
-      await deployZkSyncEra(taskArgs)
-    })
+  .addParam('privateKey', 'Private key used to deploy')
+  .addParam('params', 'Path to params json')
+  .addFlag('verify', 'Whether verify the contracts')
+  .setAction(async (taskArgs) => {
+    await deployZkSyncEra(taskArgs)
+  })
 
 const DEFAULT_COMPILER_SETTINGS = {
   version: '0.8.17',
@@ -35,21 +39,21 @@ export default {
   },
   networks: {
     zkSyncLocalhost: {
-      url: 'http://localhost:3050',
-      ethNetwork: 'http://localhost:8545',
+      url: process.env.ZKSYNC_WEB3_API_URL,
+      ethNetwork: '',
       zksync: true,
     },
     zkSyncTestnet: {
       url: 'https://testnet.era.zksync.dev',
       ethNetwork: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
       zksync: true,
-      verifyURL: 'https://zksync2-testnet-explorer.zksync.dev/contract_verification'
+      verifyURL: 'https://zksync2-testnet-explorer.zksync.dev/contract_verification',
     },
     zkSyncMainnet: {
       url: 'https://mainnet.era.zksync.io',
       ethNetwork: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
       zksync: true,
-      verifyURL: 'https://zksync2-mainnet-explorer.zksync.io/contract_verification'
+      verifyURL: 'https://zksync2-mainnet-explorer.zksync.io/contract_verification',
     },
   },
   defaultNetwork: 'zkSyncLocalhost',
@@ -65,6 +69,6 @@ export default {
     settings: {},
   },
   mocha: {
-    timeout: 60000,
+    timeout: 100000,
   },
 }
